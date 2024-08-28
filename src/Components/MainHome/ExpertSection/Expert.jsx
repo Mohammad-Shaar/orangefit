1c9ed576ee1a59ddba4ce2cffefc065bdcd97c19
Mohammad Shaar <137814293+Mohammad-Shaar@users.noreply.github.com>
2024-08-28 15:55:31 +0300

    working on expert section

--- src/Components/MainHome/ExpertSection/Expert.jsx
+++ src/Components/MainHome/ExpertSection/Expert.jsx
@@ -16,19 +16,30 @@ const Expert = () => {
           <p className="font-bold text-4xl z-20">Meet our expert</p>
           <p className="font-bold text-7xl mt-4 z-20">Dr.MICHEL ADIB</p>
           <p className="font-bold text-2xl mt-20 mb-6">My Philosophy</p>
-          <p className="text-2xl leading-[27px]">
+          <p className="font-bold text-[27px] leading-[32px] mb-6">
             “My goal is to let everybody get the healthy life they deserve , no
-            matter where they are from or how much money they have” I know how
-            much you suffered , I know how many diets you tried, to reach the
-            body you deserve and failed, The problem is not you, The problem is
-            the boring food , hunger in the diets you chose. Orangefit is the
-            solution .
+            matter where they are from or how much money they have”
+          </p>
+          <p className="leading-[27px] text-xl">
+            I know how much you suffered ,<br /> I know how many diets you
+            tried,
+            <br /> to reach the body you deserve and failed,
+            <br /> The problem is not you,
+            <br /> The problem is the boring food , hunger in the diets you
+            chose.
+            <br />
+            <span className="font-semibold text-xl">
+              Orangefit is the solution .
+            </span>
+          </p>
+          <p className="mt-4 text-base text-[#F6FFF8B2]">
+            Michel Adib , Founder of orangefit.
           </p>
           <div className="mt-6">
             <LinkBtn path={"#"}>start today</LinkBtn>
           </div>
         </div>
-        <div className="max-w-[35%]">
+        <div className="max-w-[40%]">
           <img src={persone} />
         </div>
       </div>
