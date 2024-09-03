f160a46ace075a71b84ef69a5870ed4ce4393ebb
Mohammad Shaar <137814293+Mohammad-Shaar@users.noreply.github.com>
2024-09-03 17:08:20 +0300

    working on responsive download

--- src/Components/MainHome/DownloadSection/Download.jsx
+++ src/Components/MainHome/DownloadSection/Download.jsx
@@ -6,15 +6,15 @@ import phone2 from "../../../assets/Phone2.png";
 const Download = () => {
   return (
     <section className="rounded-2xl mt-10 bg-gradient-to-r from-[#FF6400] via-[#FF6400] to-[#FE8E22] md:mt-0">
-      <div className="container flex items-center justify-between space-x-20 mx-auto p-6 py-10 text-gray-50">
-        <div className="font-semibold max-w-[56%]">
-          <p className=" text-6xl leading-[70px]">
+      <div className="container flex flex-col items-center justify-between mx-auto p-6 py-24 text-gray-50 md:flex-row md:space-x-20 md:py-10">
+        <div className="font-semibold text-center md:text-left md:max-w-[56%]">
+          <p className="font-bold text-4xl leading-[46px] md:text-6xl md:leading-[70px]">
             Level Up Your Fitness Journey With Orangefit App.
           </p>
-          <p className="text-2xl mt-10">
+          <p className="text-xl leading-8 md:text-2xl mt-10">
             The only app you will need to reach your dream body.
           </p>
-          <div className="flex space-x-8 mt-6">
+          <div className="hidden items-center gap-5 mt-6 md:flex">
             <div>
               <img src={ph1} />
             </div>
@@ -23,12 +23,20 @@ const Download = () => {
             </div>
           </div>
         </div>
-        <div className="flex items-end relative">
+        <div className="flex items-end relative py-8 md:py-0">
           <div className="z-10">
             <img src={phone1} />
           </div>
           <div className="relative right-10">
             <img src={phone2} />
+          </div>
+        </div>
+        <div className="flex justify-center items-center flex-wrap gap-5 mt-6 md:hidden">
+          <div>
+            <img src={ph1} />
+          </div>
+          <div>
+            <img src={ph2} />
           </div>
         </div>
       </div>
