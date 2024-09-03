465f20dc4e494b97612607b5e3af48bc5c4e15af
Mohammad Shaar <137814293+Mohammad-Shaar@users.noreply.github.com>
2024-09-03 16:19:56 +0300

    using new dependencies for scrolling

--- src/Components/MainHome/ResultsSction/Results.jsx
+++ src/Components/MainHome/ResultsSction/Results.jsx
@@ -1,60 +1,56 @@
-import { useState } from "react";
 import CountUp from "react-countup";
-import ScrollTrigger from "react-scroll-trigger";
+import { useInView } from "react-intersection-observer";
 
 const Results = () => {
-  const [counterState, setCounterState] = useState(false);
+  const { ref, inView } = useInView({
+    triggerOnce: true, // The animation will only trigger once
+  });
 
   return (
     <section id="results">
-      <ScrollTrigger
-        onEnter={() => setCounterState(true)}
-        onExit={() => setCounterState(false)}
-      >
-        <div className="container mx-auto p-6">
-          <h1 className="font-bold text-2xl text-center text-orange-600 mt-6 p-6 md:text-4xl">
-            What We've Been Up To ...
-          </h1>
-          <div className="flex flex-col gap-14 justify-center text-gray-50 font-bold my-12 md:flex-row md:gap-20 md:flex-wrap md:mb-32 md:mt-20">
-            <div className="flex flex-col items-center">
-              <p className="text-6xl">
-                {counterState && (
-                  <CountUp start={0} end={1200} duration={3.75}></CountUp>
-                )}
-                +
-              </p>
-              <p>Happy clients</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <p className="text-6xl">
-                {counterState && (
-                  <CountUp start={0} end={110} duration={3.75}></CountUp>
-                )}
-                %
-              </p>
-              <p>guaranteed results</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <p className="text-6xl">
-                {counterState && (
-                  <CountUp start={0} end={300} duration={3.75}></CountUp>
-                )}
-                +K
-              </p>
-              <p>workouts</p>
-            </div>
-            <div className="flex flex-col items-center">
-              <p className="text-6xl">
-                {counterState && (
-                  <CountUp start={0} end={500} duration={3.75}></CountUp>
-                )}
-                +
-              </p>
-              <p>Healthy Recipes</p>
-            </div>
+      <div className="container mx-auto p-6">
+        <h1 className="font-bold text-2xl text-center text-orange-600 mt-6 p-6 md:text-4xl">
+          What We&apos;ve Been Up To ...
+        </h1>
+        <div className="flex flex-col gap-14 justify-center text-gray-50 font-bold my-12 md:flex-row md:gap-20 md:flex-wrap md:mb-32 md:mt-20">
+          <div className="flex flex-col items-center">
+            <p ref={ref} className="text-6xl">
+              {inView && (
+                <CountUp start={0} end={1200} duration={3.75}></CountUp>
+              )}
+              +
+            </p>
+            <p>Happy clients</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p ref={ref} className="text-6xl">
+              {inView && (
+                <CountUp start={0} end={110} duration={3.75}></CountUp>
+              )}
+              %
+            </p>
+            <p>guaranteed results</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p ref={ref} className="text-6xl">
+              {inView && (
+                <CountUp start={0} end={300} duration={3.75}></CountUp>
+              )}
+              +K
+            </p>
+            <p>workouts</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p ref={ref} className="text-6xl">
+              {inView && (
+                <CountUp start={0} end={500} duration={3.75}></CountUp>
+              )}
+              +
+            </p>
+            <p>Healthy Recipes</p>
           </div>
         </div>
-      </ScrollTrigger>
+      </div>
     </section>
   );
 };
