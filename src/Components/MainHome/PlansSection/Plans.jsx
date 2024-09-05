a4a9c9dc1a9cb3693a5178f4539ecdb93bd6efad
Mohammad Shaar <137814293+Mohammad-Shaar@users.noreply.github.com>
2024-09-05 14:52:18 +0300

    improving plans section

--- src/Components/MainHome/PlansSection/Plans.jsx
+++ src/Components/MainHome/PlansSection/Plans.jsx
@@ -1,3 +1,11 @@
+import SinglePlan from "./SinglePlan";
+
+const PLANSDATA = [
+  { type: "Diet only", price: "25" },
+  { type: "Diet + Workout", price: "19", popular: true },
+  { type: "Workout only", price: "25" },
+];
+
 const Plans = () => {
   return (
     <section className="hidden md:block">
@@ -6,39 +14,14 @@ const Plans = () => {
           Choose a plan that’s right for you
         </h1>
         <div className="flex justify-between items-center">
-          <div className="flex flex-col items-center space-y-4 p-12 pt-20 w-[30%] rounded-xl h-full bg-custom-dark border-4 border-custom-dark duration-300 hover:border-orange-600">
-            <h2 className="font-bold text-2xl">Diet only</h2>
-            <p className="text-lg text-orange-400">/user/mo</p>
-            <p className="flex pt-1 font-bold text-6xl">
-              <sup className="text-2xl">$</sup>25
-            </p>
-            <button className="bg-[#FD7B03] py-3 px-14 text-lg rounded-lg ">
-              Subscribe
-            </button>
-          </div>
-          <div className="relative flex flex-col items-center space-y-4 p-12 pt-20 w-[30%] rounded-xl h-full bg-custom-dark border-4 border-custom-dark duration-300 hover:border-orange-600">
-            <h2 className="font-bold text-2xl">Diet + Workout</h2>
-            <p className="text-lg text-orange-400">/user/mo</p>
-            <p className="flex pt-1 font-bold text-6xl">
-              <sup className="text-2xl">$</sup>19
-            </p>
-            <button className="bg-[#FD7B03] py-3 px-14 text-lg rounded-lg ">
-              Subscribe
-            </button>
-            <p className="absolute text-center -top-5 w-custom py-3 font-bold text-2xl rounded-t-xl bg-gradient-to-r from-[#FF6400] via-[#FF6400] to-[#FE8E22]">
-              Most popular
-            </p>
-          </div>
-          <div className="flex flex-col items-center space-y-4 p-12 pt-20 w-[30%] rounded-xl h-full bg-custom-dark border-4 border-custom-dark duration-300 hover:border-orange-600">
-            <h2 className="font-bold text-2xl">Workout only</h2>
-            <p className="text-lg text-orange-400">/user/mo</p>
-            <p className="flex pt-1 font-bold text-6xl">
-              <sup className="text-2xl">$</sup>25
-            </p>
-            <button className="bg-[#FD7B03] py-3 px-14 text-lg rounded-lg ">
-              Subscribe
-            </button>
-          </div>
+          {PLANSDATA.map((item, index) => (
+            <SinglePlan
+              key={index}
+              type={item.type}
+              price={item.price}
+              popular={item.popular}
+            />
+          ))}
         </div>
         <hr className="orange-radial" />
       </div>
---
+++ src/Components/MainHome/PlansSection/SinglePlan.jsx
@@ -0,0 +1,22 @@
+const SinglePlan = ({ type, price, popular }) => {
+  return (
+    <div className="relative flex flex-col items-center space-y-4 p-12 pt-20 w-[30%] rounded-xl h-full bg-custom-dark border-4 border-custom-dark duration-300 hover:border-orange-600">
+      <h2 className="font-bold text-2xl">{type}</h2>
+      <p className="text-lg text-orange-400">/user/mo</p>
+      <p className="flex pt-1 font-bold text-6xl">
+        <sup className="text-2xl">$</sup>
+        {price}
+      </p>
+      <button className="bg-[#FD7B03] py-3 px-14 text-lg rounded-lg ">
+        Subscribe
+      </button>
+      {popular && (
+        <p className="absolute text-center -top-5 w-custom py-3 font-bold text-2xl rounded-t-xl bg-gradient-to-r from-[#FF6400] via-[#FF6400] to-[#FE8E22]">
+          Most popular
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default SinglePlan;
